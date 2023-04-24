c3c042cdf32f186c6d2dbc4502281b1fae0c4080
Prem Chaitanya Prathi <chaitanyaprem@gmail.com>
2023-04-24 16:58:50 +0530

    Fixed a bug in tree generation. Implemented generating and veriying proofs

--- merkletree/merkletree.go
+++ merkletree/merkletree.go
@@ -1,6 +1,7 @@
 package merkletree
 
 import (
+	"bytes"
 	"fmt"
 	"math"
 )
@@ -25,18 +26,26 @@ type Node struct {
 	//TODO: Add Path metadata
 }
 
-func (n *Node) String() string {
-	fmtNode := ""
+type Proof struct {
+	Hashes  [][]byte
+	Indexes []int
+}
 
+func (n *Node) String() string {
+	fmtNode := fmt.Sprintf("Node : {Hash: %v, parent %v, IsLeaf:%t, IsDuplicate: %t}\n",
+		n.Hash, n.Parent, n.IsLeaf, n.IsDuplicate)
 	return fmtNode
 }
 
-func NewTree(data [][]byte, hashFunc HashFunction) (*MerkleTree, error) {
+type Data [][]byte
+
+func NewTree(data Data, hashFunc HashFunction) (*MerkleTree, error) {
 	var tree MerkleTree
 	tree.HashFunc = hashFunc
+
 	var err error
 	leafCount := len(data)
-	fmt.Println(leafCount, " is leaf node count")
+	fmt.Println("Number of leaves:", leafCount)
 	if leafCount == 0 {
 		return nil, fmt.Errorf("error: cannot build a merkle tree without any data")
 	}
@@ -44,6 +53,7 @@ func NewTree(data [][]byte, hashFunc HashFunction) (*MerkleTree, error) {
 	if err != nil {
 		return nil, err
 	}
+	leafCount = len(tree.Leaves)
 	//Calculate Tree depth based on number of leaves considering it is a binary hash tree
 	tree.Depth = int(math.Log2(float64(leafCount)))
 	tree.Root, err = buildIntermediateLevel(tree.Leaves, &tree)
@@ -53,7 +63,7 @@ func NewTree(data [][]byte, hashFunc HashFunction) (*MerkleTree, error) {
 	return &tree, nil
 }
 
-func populateLeaves(data [][]byte, tree *MerkleTree) error {
+func populateLeaves(data Data, tree *MerkleTree) error {
 	leafCount := len(data)
 
 	//TODO : sort leaves and then build tree so that same root hash is generated even if data order is different.
@@ -63,6 +73,7 @@ func populateLeaves(data [][]byte, tree *MerkleTree) error {
 		if err != nil {
 			return err
 		}
+
 		tree.Leaves = append(tree.Leaves, node)
 	}
 
@@ -75,6 +86,7 @@ func populateLeaves(data [][]byte, tree *MerkleTree) error {
 		}
 		tree.Leaves = append(tree.Leaves, node)
 	}
+
 	return nil
 }
 
@@ -82,7 +94,6 @@ func buildLeafNode(data []byte, tree *MerkleTree) (*Node, error) {
 	var node Node
 	var err error
 	node.Data = data
-	//TODO: What if hashfunction returns error.
 	node.Hash, err = tree.HashFunc(node.Data)
 	if err != nil {
 		return nil, err
@@ -92,13 +103,17 @@ func buildLeafNode(data []byte, tree *MerkleTree) (*Node, error) {
 }
 
 func buildIntermediateLevel(nodes []*Node, tree *MerkleTree) (*Node, error) {
-	var levelNodes []*Node
+	//var levelNodes []*Node
+	levelNodes := make([]*Node, len(nodes)/2)
+	levelIndex := 0
 	for j := 0; j < len(nodes); j = j + 2 {
-		node, err := createNonLeafNode(tree.Leaves[j], tree.Leaves[j+1], tree.HashFunc)
+		node, err := createNonLeafNode(nodes[j], nodes[j+1], tree.HashFunc)
 		if err != nil {
 			return nil, err
 		}
-		levelNodes = append(levelNodes, node)
+		levelNodes[levelIndex] = node
+		levelIndex++
+		//levelNodes = append(levelNodes, node)
 		if len(nodes) == 2 {
 			return node, nil
 		}
@@ -124,29 +139,90 @@ func (t *MerkleTree) RootHash() []byte {
 	return t.Root.Hash
 }
 
-func (t *MerkleTree) GenerateMerkleProof() {
-	fmt.Println("GenerateMerkleProof:To be implemented")
+func (t *MerkleTree) UpdateLeaf(proof *Proof, newValue []byte) error {
+	fmt.Println("UpdateLeaf:To be implemented")
 
+	return nil
 }
 
-func (t *MerkleTree) VerifyData(data []byte) (bool, error) {
-	fmt.Println("VerifyData:To be implemented")
+func (t *MerkleTree) GenerateMerkleProof(data []byte) (*Proof, error) {
+	dHash, err := t.HashFunc(data)
+	if err != nil {
+		return nil, err
+	}
+	var proof Proof
+	//TODO: Optimize
+	//proof.Hashes = make([][]byte, t.Depth)
+	//Traverse the tree upwards and get hashes and indexes required for the proof.
+	for _, node := range t.Leaves {
+		if bytes.Equal(node.Hash, dHash) {
+			curHash := dHash
+			parent := node.Parent
+			for parent != nil {
+				if bytes.Equal(parent.Left.Hash, curHash) { //Left Node
+					proof.Hashes = append(proof.Hashes, parent.Right.Hash)
+					proof.Indexes = append(proof.Indexes, 1)
+				} else { //Right Node
+					proof.Hashes = append(proof.Hashes, parent.Left.Hash)
+					proof.Indexes = append(proof.Indexes, 0)
+				}
+				curHash = parent.Hash
+				parent = parent.Parent
+			}
+			break
+		}
+	}
+	//fmt.Printf("Generated Proof for has %v is %v \n", dHash, proof)
+	return &proof, nil
+}
+
+func (t *MerkleTree) VerifyProof(data []byte, proof *Proof) (bool, error) {
+	//fmt.Println("VerifyProof:To be implemented")
+	dHash, err := t.HashFunc(data)
+	if err != nil {
+		return false, err
+	}
+	for i, val := range proof.Hashes {
+
+		if proof.Indexes[i] == 0 {
+			dHash, err = t.HashFunc(append(val, dHash...))
+			if err != nil {
+				return false, err
+			}
+		} else {
+			dHash, err = t.HashFunc(append(dHash, val...))
+			if err != nil {
+				return false, err
+			}
+		}
+	}
+	// fmt.Println("generated rootHash is ", dHash)
+	// fmt.Println("Tree's rootHash is ", t.RootHash())
+
+	if !bytes.Equal(dHash, t.RootHash()) {
+		return false, fmt.Errorf("proof verification failed due to mismatch in generated root hash")
+	}
 	return true, nil
 }
 
 func (t *MerkleTree) VerifyTree() (bool, error) {
-	fmt.Println("VerifyTree:To be implemented")
-
+	fmt.Println("VerifyTree:Verifying proofs of all leaves")
+	//TODO: Generate rootHash and verifying it against stored root.
 	return true, nil
 }
 
 func (m *MerkleTree) String() string {
 	fmtTree := ""
-	//TODO: Traverse the tree and invoke string on each node.
-	node := m.Root
-	for node != nil {
-		fmtTree += node.String()
-		node = node.Left
+	//TODO: Print the entire tree structure with all nodes.
+	/* 	node := m.Root
+	   	for node != nil {
+	   		fmtTree += node.String()
+	   		node = node.Left
+	   	} */
+	leaves := ""
+	for i, val := range m.Leaves {
+		leaves += fmt.Sprintf("%d,{%v}\n", i, val)
 	}
+	fmtTree = fmt.Sprintf("Depth: %d, \nRoot:%v,\n Leaves:%s", m.Depth, m.Root, leaves)
 	return fmtTree
 }
--- merkletree/merkletree_test.go
+++ merkletree/merkletree_test.go
@@ -1,6 +1,7 @@
 package merkletree
 
 import (
+	"bytes"
 	"crypto/sha256"
 	"fmt"
 	"testing"
@@ -10,12 +11,13 @@ func HashFuncSHA256(data []byte) ([]byte, error) {
 	hash := sha256.New()
 	hash.Write(data)
 	hashValue := hash.Sum(nil)
-	fmt.Println("Calculated hash is :", hashValue)
+	//fmt.Println("Calculated hash is :", hashValue)
 	return hashValue, nil
 }
 
 func TestMerkleBasics(t *testing.T) {
 	var data [][]byte
+	expectedHash := []byte{95, 48, 204, 128, 19, 59, 147, 148, 21, 110, 36, 178, 51, 240, 196, 190, 50, 178, 78, 68, 187, 51, 129, 240, 44, 123, 165, 38, 25, 208, 254, 188}
 	strings := []string{
 		"Hello",
 		"Hi",
@@ -33,5 +35,26 @@ func TestMerkleBasics(t *testing.T) {
 		t.FailNow()
 	}
 	//TODO: Add a verification for expected rootHash for input data.
-	fmt.Println("root hash is ", tree.RootHash())
+	//fmt.Println("root hash is ", tree.RootHash())
+	fmt.Printf("Tree : \n %v", tree)
+	if !bytes.Equal(tree.RootHash(), expectedHash) {
+		fmt.Println("Root hash generated is not matching expected hash")
+		t.FailNow()
+	}
+	proof, err := tree.GenerateMerkleProof(data[0])
+	if err != nil {
+		fmt.Println("Failed to generate Merkle Proof due to error ", err)
+		t.FailNow()
+	}
+	fmt.Println("Proof generated is : \n", proof)
+
+	verified, err := tree.VerifyProof(data[0], proof)
+	if err != nil {
+		fmt.Println("Failed to verify Merkle Proof due to error ", err)
+		t.FailNow()
+	}
+	if !verified {
+		fmt.Println("Invalid proof for the data", err)
+		t.FailNow()
+	}
 }
